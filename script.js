b0fab53b7da938051394f718250e1e8784a658a4
PsOlga <psyrniko1@gmail.com>
2024-07-24 22:47:40 +0200

    Added logic for event list: script js file, added event array and created DOM element with given parameters. The header of the main container of the second page has been corrected.

--- script.js
+++ script.js
@@ -1,71 +1,70 @@
-// import { eventsStore } from "./scripts/data-grid.js";
-// import { createDomElement } from "./scripts/utils.js";
-// import { formatDate } from "./scripts/utils.js";
-
-// const eventTipe = document.querySelector("#event-type");
-// const eventDistance = document.querySelector("#event-distance");
-// const eventCategory = document.querySelector("#event-category");
-
-// const eventsFilter = [
-//     {}
-// ]
+import { eventsStore } from "./scripts/data.js";
+import { createDomElement, formatDate} from "./scripts/utils.js";
 
 
+const Eventses= document.querySelector(".filters-events")
+const eventTypeSelect = document.getElementById("event-type")
+const eventDistanceSelect = document.getElementById("event-distance")
+const eventCategorySelect = document.getElementById("event-category")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const events = [
-    { name: 'Event 1', distance: 5, category: 'Social Activities' },
-    { name: 'Event 2', distance: 10, category: 'Hobbies and Passions' },
-    { name: 'Event 3', distance: 15, category: 'Health and Wellbeing' },
-    { name: 'Event 4', distance: 25, category: 'Business' },
-    { name: 'Event 5', distance: 50, category: 'Technology' }
-];
-
-function filterEvents() {
-    const selectedDistance = document.getElementById('event-distance').value;
-    const selectedCategory = document.getElementById('event-category').value;
-
-    const filteredEvents = events.filter(event => {
-        const distanceMatch = selectedDistance === 'any' || event.distance == selectedDistance;
-        const categoryMatch = selectedCategory === 'any' || event.category === selectedCategory;
-        return distanceMatch && categoryMatch;
-    });
-
-    displayEvents(filteredEvents);
+function createEvent(arr) {
+  arr.forEach((eventElement) => {
+    const eventItem = createDomElement({ tag: "a", className: "events-link", href: "#" })
+    Eventses.append(eventItem)
+    const eventsImages = createDomElement({ tag: "div", className: "events-images-alls" })
+    eventItem.append(eventsImages)
+    const eventImage = createDomElement({ tag: "img", className: "events-images-alls", src: eventElement.image })
+    eventsImages.append(eventImage)
+    const eventDetails = createDomElement({ tag: "div", className: "events-all-details" })
+    eventItem.append(eventDetails)
+    const eventsDate = createDomElement({ tag: "p", className: "events-all-date", textValue: formatDate(eventElement.date) })
+    const eventsHeader = createDomElement({ tag: "h3", className: "events-haeder-text", textValue: eventElement.title })
+    const eventsCategory = createDomElement({ tag: "p", className: "events-all-category", textValue: eventElement.category })
+    eventDetails.append(eventsDate,eventsHeader , eventsCategory);
+    if (eventElement.type === "online") {
+      const onlineEventImage = createDomElement({
+        tag: "img",
+        className: "events-all-online",
+        src: "./image-events-filter/online-event.svg",
+        alt: "online event",
+      })
+      eventDetails.append(onlineEventImage)
+    }
+    if (eventElement.attendsInfo) {
+      const eventsInfo = createDomElement({
+        tag: "p",
+        className: "events-info-container-attendsInfo",
+        textValue:`${eventElement.attendsInfo} attendsInfo`,
+      })
+      eventDetails.append(eventsInfo)
+    }
+  })
 }
-
-function displayEvents(events) {
-    const eventList = document.getElementById('event-list');
-    eventList.innerHTML = events.map(event => `<div>${event.name} - ${event.distance} km - ${event.category}</div>`).join('');
+function clearEvents() {
+  while (Eventses.firstChild) {
+    Eventses.removeChild(Eventses.firstChild);
+  }
 }
+function filterEvents(arr) {
+  const selectedType = eventTypeSelect.value !== "any" && eventTypeSelect.value
+  const selectedDistance = eventDistanceSelect.value !== "any" && eventDistanceSelect.value
+  const selectedCategory = eventCategorySelect.value !== "any" && eventCategorySelect.value
+  let filteredArr = arr
+  if (selectedType) {
+    filteredArr = filteredArr.filter((element) => element.type === selectedType)
+  }
+  if (selectedDistance) {
+    filteredArr = filteredArr.filter((element) => String(element.distance) === selectedDistance)
+  }
+  if (selectedCategory) {
+    filteredArr = filteredArr.filter((element) => element.category === selectedCategory)
+  }
+  clearEvents()
+  createEvent(filteredArr)
+}
+eventTypeSelect.addEventListener("change", () => {filterEvents(eventsStore)})
+eventDistanceSelect.addEventListener("change", () => {filterEvents(eventsStore)})
+eventCategorySelect.addEventListener("change", () => {filterEvents(eventsStore)})
+createEvent(eventsStore)
 
-document.getElementById('event-distance').addEventListener('change', filterEvents);
-document.getElementById('event-category').addEventListener('change', filterEvents);
-
-// Initial display
-displayEvents(events);
-0
